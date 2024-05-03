adc39bf7791ef6d66aba1cb8e33d56b79ba28cab
sushicab <63993657+sushicab@users.noreply.github.com>
2024-05-03 23:14:37 +0900

    work

---
+++ components/AR.jsx
@@ -0,0 +1,226 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <div className="AR-container">
+        <iframe
+          src="https://player.vimeo.com/video/581720024?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+          allow="autoplay; fullscreen; picture-in-picture"
+          title="Vimeo video"
+        ></iframe>
+      </div>
+
+      <div className="AR-text-content">
+        <div className="AR-title">
+          <h2>
+            Augmented Reality Projects
+            <br />
+            <br />
+            1. Gender Shell
+          </h2>
+        </div>
+        <p className="AR-text">
+          “Gender Shell” is an AR installation to visualize gender
+          classification experience, as one of the three Reflective design and
+          social experiment installations in the research project Labeliing: The
+          Reflective Design of Socialized Gender Classification.
+          <br />
+          <br />
+          <br />
+          Installation 1: "Gender Shell`
+          <br />
+          <br />
+          <br />
+          Technology comes from humanity, what it build is a reflection of
+          majority view. In this premise, what "body shell" (should) we be? What
+          will majority of databases thinks about our biological appearance?
+          Facing the reflection of social expectation, Which "gender shell"
+          would our biological sex be linked to? Based on the concept of this
+          work above, participant’s gender will be classified by an unsupervised
+          machine learning algorithm, that shows the participant’s picture
+          together with a mosaic of faces it thinks it’s similar. Showing how,
+          without using human made labels of faces from a dataset, the machine
+          would divide these faces into groups.
+          <br />
+          <br />
+          <br />
+          We collected 61 participants’ answers about what is the first
+          description pop up in their minds when they saw the words ‘Male’ and
+          ‘Female’. We used a questionnaire to collect answers to try to avoid
+          bias.
+          <br />
+          <br />
+          <br />
+          In this setup, answers appear as text in a Windows 2007 style when the
+          camera detects the participant's face. Random facial expressions
+          trigger different responses on-screen. The background features a
+          semi-transparent glitch texture, blurring the line between reality and
+          virtual. The mosaic mask design, inspired by face recognition,
+          transitions from blurry to clear as faces are detected, symbolizing
+          equality amidst gender classification.
+        </p>
+      </div>
+      <div className="AR-image-iteme-grid">
+        <div className="AR-image-item">
+          <img
+            src="/img/projects/interactive design/gender shell/gender2.jpg"
+            alt="Image 3"
+            className="AR-image"
+          />
+          <iframe
+            src="https://player.vimeo.com/video/581720024?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+            className="AR-image"
+            allow="autoplay; fullscreen; picture-in-picture"
+            title="Vimeo video"
+          ></iframe>
+          <img
+            src="/img/projects/interactive design/gender shell/gender1.png"
+            alt="Image 2"
+            className="AR-image"
+          />
+        </div>
+      </div>
+
+      <div className="AR-credits-container">
+        <div>
+          <h2 className="text-1xl font-bold mb-4">
+            More details in related publication:
+          </h2>
+          <div className="AR-credits">
+            <p>
+              <a
+                href="https://dl.acm.org/doi/abs/10.1145/3393914.3395900"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "underline" }}
+              >
+                [1]Labeling: The Reflective Design of Socialized Gender
+                Classification
+              </a>
+            </p>
+          </div>
+          <p className="AR-credits">C R E D I T S</p>
+          <p className="AR-credits">
+            CHIA-HSIN CHEN <strong> Lead Designer/ Researcher </strong>{" "}
+          </p>
+          <p className="AR-credits">
+            YIFAN ZHUANG <strong> AR Creator/ Developer </strong>{" "}
+          </p>
+        </div>
+      </div>
+
+      <h2 className="AR-title">2. AR Filter Collection</h2>
+      <div className="AR-text-content">
+        <p className="AR-text">
+          In today's digital age, social media platforms have become vibrant
+          hubs for self-expression and creativity. One of the most engaging
+          forms of expression on these platforms is the use of augmented reality
+          (AR) filters, which allow users to transform their photos and videos
+          with interactive and playful effects.
+          <br />
+          <br />
+          <br />
+          An experimental project to explore the expanded cyber identity, all
+          filters designed and developed by me.
+          <br />
+          <br />
+          <br />
+          <br />
+          <strong>
+            <em> - One More Robot </em>
+          </strong>
+          <br />
+          <br />
+          Influenced by "Ghost in the Shell," I found myself drawn to the idea
+          of exploring the world of cybernetics. With a longing to understand
+          this realm, I experimented with creating an augmented reality effect
+          that splits the face.
+        </p>{" "}
+      </div>
+      <div className="AR-image-item">
+        <img
+          src="/img/projects/interactive design/gender shell/cover.jpg"
+          alt="Image 2"
+          className="AR-image"
+        />
+        <iframe
+          src="https://player.vimeo.com/video/581701361?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          className="AR-image"
+          allow="autoplay; fullscreen; picture-in-picture"
+          title="Vimeo video"
+        ></iframe>
+        <img
+          src="/img/projects/interactive design/gender shell/2.png"
+          alt="Image 1"
+          className="AR-image"
+        />
+      </div>
+      <div className="AR-text-content">
+        <p className="AR-text">
+          <strong>
+            <em> - /ǫᴎiɘdoɘᴎ/ </em>
+          </strong>
+          <br />
+          <br />
+          "Neo being: Reflections on Growth" offers glimpses into the lives of
+          humans living in the futuristic world of 2047.
+          <br />
+          <br />
+          Featuring poignant screen text quotes from one of my favorite sci-fi
+          movies, "2001: A Space Odyssey," the experience begins with the iconic
+          words:
+          <br />
+          <br />
+          "Good afternoon, gentlemen.
+          <br />
+          <br />
+          I am a HAL 9000 computer.
+          <br />
+          <br />
+          I became operational at the H.A.L. plant in Urbana, Illinois on the
+          12th of January 1992."
+          <br />
+          <br />
+          Face cover was selected to use transparent half concealed metal mask
+          with two lights reflecting on. Black stone around ear was paying
+          tribute  to monolith in the movie<em>2001; A  Space Odyssey</em>, as
+          the enlightenment to encourage humankind pursue the technological
+          development and space travel.
+        </p>{" "}
+      </div>
+      <div className="AR-image-item">
+        <img
+          src="/img/projects/interactive design/gender shell/icon.jpg"
+          alt="Image 3"
+          className="AR-image"
+        />
+        <iframe
+          src="https://player.vimeo.com/video/581714257?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          className="AR-image"
+          allow="autoplay; fullscreen; picture-in-picture"
+          title="Vimeo video"
+        ></iframe>
+        <img
+          src="/img/projects/interactive design/gender shell/1.png"
+          alt="Image 2"
+          className="AR-image"
+        />
+      </div>
+      <div className="AR-credits-container">
+        <div className="AR-credits">
+          <p>C R E D I T S</p>
+          <p>
+            YIFAN ZHUANG <strong> Designer/ Developer </strong>{" "}
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AboutPage;
