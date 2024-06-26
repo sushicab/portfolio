bf50b68518f60e5254fb071f0d7aaa1710c65353
sushicab <63993657+sushicab@users.noreply.github.com>
2024-06-26 20:29:35 +0900

    favicon change

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,11 @@ export const metadata: Metadata = {
   description: "portfolio。",
   icons: {
     icon: [
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/public/favicon/favicon-32x32.png",
-      },
+      { rel: "icon", url: "/favicon.ico" },
+      { rel: "icon", url: "/favicon-16x16.png", sizes: "16x16" },
+      { rel: "icon", url: "/favicon-32x32.png", sizes: "32x32" },
+      { rel: "icon", url: "/favicon-64x64.png", sizes: "64x64" },
+      { rel: "icon", url: "/favicon-128x128.png", sizes: "128x128" },
     ],
   },
 };
