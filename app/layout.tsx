55dfb3e05ae54b10fa481f0345c298d4140b675a
sushicab <63993657+sushicab@users.noreply.github.com>
2024-07-15 16:13:06 +0900

    favicon_update3

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,12 @@ export const metadata: Metadata = {
   title: "YIFAN ZHUANG",
   description: "portfolio。",
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/favicon-32x32.png",
-    other: [
-      { rel: "icon", url: "/favicon-64x64.png", sizes: "64x64" },
-      { rel: "icon", url: "/favicon-128x128.png", sizes: "128x128" },
+    icon: [
+      { rel: "icon", href: "/favicon.ico" },
+      { rel: "icon", href: "/favicon-16x16.png", sizes: "16x16" },
+      { rel: "icon", href: "/favicon-32x32.png", sizes: "32x32" },
+      { rel: "icon", href: "/android-chrome-192x192.png", sizes: "192x192" },
+      { rel: "icon", href: "/apple-touch-icon.png", sizes: "180x180" },
     ],
   },
 };
@@ -27,11 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" href="/favicon-32x32.png" />
-        <link rel="icon" sizes="64x64" href="/favicon-64x64.png" />
-        <link rel="icon" sizes="128x128" href="/favicon-128x128.png" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" href="/android-chrome-192x192.png" sizes="192x192" />
+        <link rel="icon" href="/apple-touch-icon.png" sizes="180x180" />
       </head>
       <body
         style={{
